a761dd559ad65ff21fd1fb8c768aa9df2f9cbc44
ntlind <nthorlind@gmail.com>
2020-08-11 18:20:05 -0400

    Update timeseries.py

--- python_utilities/timeseries.py
+++ python_utilities/timeseries.py
@@ -13,14 +13,14 @@ def update_time(datetime_series, adjustment, time_unit="weeks"):
     return adjusted_series
 
 
-def fill_missing_timeseries(pandas_df: pd.DataFrame, grouping_cols, 
-                            freq='D', datetime_col="datetime"):
+def fill_missing_timeseries(pandas_df: pd.DataFrame, grouping_cols,
+                            datetime_col, freq='D'):
     """
     Fills-in missing dates in a pandas df with a time-series index
     """
 
     assert datetime_col in pandas_df.columns
-    assert helpers.is_datetime_series(pandas_df[datetime_col])
+    assert is_datetime_series(pandas_df[datetime_col])
 
     filled_pd = (
         pandas_df.set_index(datetime_col)
@@ -28,8 +28,8 @@ def fill_missing_timeseries(pandas_df: pd.DataFrame, grouping_cols,
         .apply(
             lambda d: d.reindex(
                 pd.date_range(
-                    pandas_df.datetime.min(),
-                    pandas_df.datetime.max(),
+                    pandas_df[datetime_col].min(),
+                    pandas_df[datetime_col].max(),
                     freq=freq
                 )
             )
@@ -40,10 +40,9 @@ def fill_missing_timeseries(pandas_df: pd.DataFrame, grouping_cols,
 
     filled_pd[grouping_cols] = filled_pd[grouping_cols].ffill(downcast="infer")
 
-    datetime_col = [col for col in list(filled_pd.columns) if "level_" in col]
-    assert len(datetime_col) == 1
+    date_col = [col for col in list(filled_pd.columns) if "level_" in col]
+    assert len(date_col) == 1
 
-    filled_pd = filled_pd.rename({datetime_col[0]: "datetime"}, axis=1)
+    filled_pd = filled_pd.rename({date_col[0]: datetime_col}, axis=1)
 
     return filled_pd
-
